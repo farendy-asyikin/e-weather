0d0c56fb1e3aac33ed4d4305a0237198983e2099
farendy-asyikin <farendy.asyikin@gmail.com>
2024-04-28 00:06:13 +0700

    feat: features sensor value

--- router/router.go
+++ router/router.go
@@ -5,14 +5,17 @@ import (
 	"gorm.io/gorm"
 	authhandler "main.go/handlers/auth"
 	devicehandler "main.go/handlers/devices"
+	sensorvaluehandler "main.go/handlers/sensor_values"
 	sensorhandler "main.go/handlers/sensors"
 	userhandler "main.go/handlers/users"
 	"main.go/middlewares"
 	devicerepository "main.go/repositories/devices"
+	sensorvaluerepository "main.go/repositories/sensor_values"
 	sensorrepository "main.go/repositories/sensors"
 	userrepository "main.go/repositories/users"
 	authservice "main.go/services/auth"
 	deviceservice "main.go/services/devices"
+	sensorvalueservice "main.go/services/sensor_values"
 	sensorservice "main.go/services/sensors"
 	userservice "main.go/services/user"
 )
@@ -20,22 +23,25 @@ import (
 func SetupRouter(r *gin.Engine, db *gorm.DB) {
 	var (
 		//repo
-		authRepository   userrepository.UserRepository     = userrepository.NewUserRepository(db)
-		userRepository   userrepository.UserRepository     = userrepository.NewUserRepository(db)
-		deviceRepository devicerepository.DeviceRepository = devicerepository.NewDeviceRepository(db)
-		sensorRepository sensorrepository.SensorRepository = sensorrepository.NewSensorRepository(db)
+		authRepository        userrepository.UserRepository               = userrepository.NewUserRepository(db)
+		userRepository        userrepository.UserRepository               = userrepository.NewUserRepository(db)
+		deviceRepository      devicerepository.DeviceRepository           = devicerepository.NewDeviceRepository(db)
+		sensorRepository      sensorrepository.SensorRepository           = sensorrepository.NewSensorRepository(db)
+		sensorValueRepository sensorvaluerepository.SensorValueRepository = sensorvaluerepository.NewSensorValueRepository(db)
 
 		//service
-		authService   authservice.AuthService     = authservice.NewAuthService(authRepository)
-		userService   userservice.UserService     = userservice.NewUserService(userRepository)
-		deviceService deviceservice.DeviceService = deviceservice.NewDeviceService(deviceRepository)
-		sensorService sensorservice.SensorService = sensorservice.NewSensorService(sensorRepository)
+		authService        authservice.AuthService               = authservice.NewAuthService(authRepository)
+		userService        userservice.UserService               = userservice.NewUserService(userRepository)
+		deviceService      deviceservice.DeviceService           = deviceservice.NewDeviceService(deviceRepository)
+		sensorService      sensorservice.SensorService           = sensorservice.NewSensorService(sensorRepository)
+		sensorValueService sensorvalueservice.SensorValueService = sensorvalueservice.NewSensorValueService(sensorValueRepository)
 
 		//handler
-		authHandler   authhandler.AuthHandler     = authhandler.NewAuthHandler(authService)
-		userHandler   userhandler.UserHandler     = userhandler.NewUserHandler(userService)
-		deviceHandler devicehandler.DeviceHandler = devicehandler.NewDeviceHandler(deviceService)
-		sensorHandler sensorhandler.SensorHandler = sensorhandler.NewSensorHandler(sensorService)
+		authHandler        authhandler.AuthHandler               = authhandler.NewAuthHandler(authService)
+		userHandler        userhandler.UserHandler               = userhandler.NewUserHandler(userService)
+		deviceHandler      devicehandler.DeviceHandler           = devicehandler.NewDeviceHandler(deviceService)
+		sensorHandler      sensorhandler.SensorHandler           = sensorhandler.NewSensorHandler(sensorService)
+		sensorValueHandler sensorvaluehandler.SensorValueHandler = sensorvaluehandler.NewSensorValueHandler(sensorValueService)
 
 		//middleware
 		authMiddleware = middlewares.AuthMiddleware()
@@ -54,25 +60,30 @@ func SetupRouter(r *gin.Engine, db *gorm.DB) {
 			userRoutes.PUT("/:id", authMiddleware, userHandler.UpdateUser)
 			userRoutes.DELETE("/:id", authMiddleware, userHandler.DeleteUserByID)
 			userRoutes.GET("/:id", authMiddleware, userHandler.GetUserByID)
-
 		}
 
 		deviceRoutes := apiRoutes.Group("devices")
 		{
-			deviceRoutes.POST("", authMiddleware, deviceHandler.CreateDevice)
-			deviceRoutes.PUT("/:id", authMiddleware, deviceHandler.UpdateDevice)
-			deviceRoutes.DELETE("/:id", authMiddleware, deviceHandler.DeleteDevice)
-			deviceRoutes.GET("/:id", authMiddleware, deviceHandler.GetDeviceByID)
-
+			deviceRoutes.POST("", deviceHandler.CreateDevice)
+			deviceRoutes.PUT("/:id", deviceHandler.UpdateDevice)
+			deviceRoutes.DELETE("/:id", deviceHandler.DeleteDevice)
+			deviceRoutes.GET("/:id", deviceHandler.GetDeviceByID)
 		}
 
 		sensorRoutes := apiRoutes.Group("sensors")
 		{
-			sensorRoutes.POST("", authMiddleware, sensorHandler.CreateSensor)
-			sensorRoutes.PUT("/:id", authMiddleware, sensorHandler.UpdateSensor)
-			sensorRoutes.DELETE("/:id", authMiddleware, sensorHandler.DeleteSensor)
-			sensorRoutes.GET("/:id", authMiddleware, sensorHandler.GetSensorByID)
+			sensorRoutes.POST("", sensorHandler.CreateSensor)
+			sensorRoutes.PUT("/:id", sensorHandler.UpdateSensor)
+			sensorRoutes.DELETE("/:id", sensorHandler.DeleteSensor)
+			sensorRoutes.GET("/:id", sensorHandler.GetSensorByID)
+		}
 
+		sensorValueRoutes := apiRoutes.Group("sensor-values")
+		{
+			sensorValueRoutes.POST("", sensorValueHandler.CreateValue)
+			sensorValueRoutes.GET("/:id", sensorValueHandler.GetValueByID)
+			sensorValueRoutes.GET("", sensorValueHandler.ListValuePagination)
+			sensorValueRoutes.POST("/bulk-delete", sensorValueHandler.BulkDeleteValue)
 		}
 
 	}
